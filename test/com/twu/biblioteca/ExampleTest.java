ceb2ddccf5dc983a68b163b4e8291b4b5c2340c3
Samantha Wardhaugh <samantha.wardhaugh@gmail.com>
2017-12-30 22:46:48 +0000

    Welcome message functionality added

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,24 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void greetingTest() {
+        OutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        System.setOut(ps);
+
+        BibliotecaApp tester = new BibliotecaApp();
+        tester.main(new String[]{});
+
+        assertEquals("Hello there! Welcome to Biblioteca!", os.toString().trim());
     }
 }
